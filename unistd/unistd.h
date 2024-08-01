47bbb9ef17975d9b657b4bec00dbde61ae96f41f
Thomas <tchoquet@student.42tokyo.jp>
2024-08-01 18:10:12 +0900

    Make cub3d working on windows and linux (#1)

---
+++ unistd/unistd.h
@@ -0,0 +1,16 @@
+/*
+ * ---------------------------------------------------
+ * unistd.h
+ *
+ * Author: Thomas Choquet <semoir.dense-0h@icloud.com>
+ * Date: 2024/08/01 15:37:47
+ * ---------------------------------------------------
+ */
+
+#ifndef UNISTD_H
+#define UNISTD_H
+
+#include <stdlib.h>
+typedef long ssize_t;
+
+#endif // UNISTD_H
